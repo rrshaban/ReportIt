0267964693f864a6954f3fd0ce1e9b45a4e2b585
Razi <rrshaban@users.noreply.github.com>
2015-01-23 21:11:27 -0500

    beginning google maps api insertion

--- start.py
+++ start.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
+appname = 'ReportIt'
+
+working = "<br />."
+
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def home():
+	return 'Welcome to %s!'+ working % appname
+
+
+@app.route('/about')
+def about():
+	return 'The about page.'
+
+@app.route('/login')
+def login():
+	return 'login'
 
 
 @app.route('/country/<country>')
 def print_country_region(country):
 	return 'Country: %s' % country
-
 
 
 @app.route('/p/<int:post_id>')
@@ -21,6 +33,11 @@ def show_post(post_id):
 	return 'Post %d' % post_id
 
 
+# with app.test_request_context():
+# 	print url_for('home')
+# 	print url_for('login', next = '/')
+
 if __name__ == '__main__':
 	app.debug = True
+	app.logger.warning('A warning occurred (%d apples)', 42)
 	app.run()
