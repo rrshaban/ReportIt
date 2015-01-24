0c5c91292f0235aa0e25bdc9a064ac8f4d8aa9f0
Razi <rrshaban@users.noreply.github.com>
2015-01-23 20:52:56 -0500

    country and post pages

--- start.py
+++ start.py
@@ -6,5 +6,21 @@ def hello_world():
 	return 'Hello World!'
 
 
+@app.route('/country/<country>')
+def print_country_region(country):
+	return 'Country: %s' % country
+
+
+
+@app.route('/p/<int:post_id>')
+def show_post(post_id):
+	####
+	# 		Post Page
+	####
+
+	return 'Post %d' % post_id
+
+
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
