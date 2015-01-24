df6d7b78817e44b622515bc48384ff9f319cf2cc
Razi <rrshaban@users.noreply.github.com>
2015-01-23 21:36:53 -0500

    google maps api working, full page

--- start.py
+++ start.py
@@ -3,12 +3,35 @@ app = Flask(__name__)
 
 appname = 'ReportIt'
 
-working = "<br />."
+workin = '''<!DOCTYPE html>
+<html>
+  <head>
+    <style type="text/css">
+      html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
+    </style>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgXKvcA_wB6l60kWyuyNWeYTP2zrVr6oo">
+    </script>
+    <script type="text/javascript">
+      function initialize() {
+        var mapOptions = {
+          center: { lat: 31.9333, lng: 35.9333},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  </head>
+  <body>
+<div id="map-canvas"></div>
+  </body>
+</html>'''
 
 @app.route('/')
 def home():
-	return 'Welcome to %s!'+ working % appname
-
+	return 'Welcome to %s!' % appname
 
 @app.route('/about')
 def about():
@@ -17,6 +40,10 @@ def about():
 @app.route('/login')
 def login():
 	return 'login'
+
+@app.route('/working')
+def working():
+	return workin
 
 
 @app.route('/country/<country>')
